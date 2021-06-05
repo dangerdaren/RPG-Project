ac9ae4d0665832ac25258cfc97f2e8234d4c44b2
Daren Stottrup <>
2021-06-04 19:04:15 -0700

    Imported Characters and animations. Replaced Player placeholder with Synty prefab. Added Animation and incorporated to movement. Modified controls in Mover.sc

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -13,8 +13,10 @@ public class FollowCamera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = target.position;
     }
+
+
 }
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,33 +6,48 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
+    private NavMeshAgent _navMeshAgent;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(1))
         {
             MoveToCurser();
         }
+        else
+        {
+            _navMeshAgent.destination = transform.position;
+        }
+
+        UpdateAnimator();
 
     }
 
     private void MoveToCurser()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit _hit;
 
-        bool hasHit = Physics.Raycast(ray, out hit);
+        bool _hasHit = Physics.Raycast(_ray, out _hit);
 
-        if (hasHit)
+        if (_hasHit)
         {
-            GetComponent<NavMeshAgent>().destination = hit.point;
+            _navMeshAgent.destination = _hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 _velocity = _navMeshAgent.velocity;
+        Vector3 _localVelocity = transform.InverseTransformDirection(_velocity);
+        float speed = _localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
     }
 }
