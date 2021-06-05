ec9740dc05f3427dc93d7dc33f225a2aee66f203
Daren Stottrup <>
2021-06-04 17:18:52 -0700

    Added Raycasts to Mover.sc. When user clicks a point on screen, the Player will navigate there. Adjusted speed of Player for better feel.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCurser();
+        }
+
+    }
+
+    private void MoveToCurser()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
