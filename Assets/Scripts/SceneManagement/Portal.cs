23408a1b97be05a6e3875c8033ba35634428f4db
Daren Stottrup <>
2021-06-24 16:52:19 -0700

    Enums are great! Utilized one on the Portal script to differentiate each portal's Origin and Destination, creating 3 variables to match up locations. This allows us to even enter doors and teleport to the same level elsewhere, or into other scenes. Theoretically even making magic doors that can create an infinite loop.

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [SerializeField] int loadScene = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier origin;
+        [SerializeField] DestinationIdentifier destination;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -21,8 +31,34 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(gameObject); // this only works when the gameobject is in the root of the scene heirarchy.
             yield return SceneManager.LoadSceneAsync(loadScene);
-            print("Scene loaded.");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.transform.position);
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                if (destination == portal.origin)
+                {
+                    return portal;
+                }
+
+            }
+
+            return null;
         }
     }
 }
