df058d39804acf1517c760137cb8c6c74759d8ba
Daren Stottrup <>
2021-07-01 18:34:25 -0700

    And now, replacing that system with more devolved version where each object is in charge of its own save state. Just beginning.

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -17,12 +17,8 @@ namespace RPG.Saving
 
             using (FileStream stream = File.Open(path, FileMode.Create))
             {
-                Transform playerTransform = GetPlayerTransform();
-                
                 BinaryFormatter formatter = new BinaryFormatter();
-                SerializableVector3 position = new SerializableVector3(playerTransform.position);
-
-                formatter.Serialize(stream, position);
+                formatter.Serialize(stream, CaptureState());
             }
 
         }
@@ -34,41 +30,35 @@ namespace RPG.Saving
 
             using (FileStream stream = File.Open(path, FileMode.Open))
             {
-                Transform playerTransform = GetPlayerTransform();
-
                 BinaryFormatter formatter = new BinaryFormatter();
-                SerializableVector3 position = (SerializableVector3)formatter.Deserialize(stream);
-
-                playerTransform.position = position.ToVector();
+                RestoreState(formatter.Deserialize(stream));
             }
 
         }
 
+        private object CaptureState()
+        {
+            Dictionary<string, object> state = new Dictionary<string, object>();
 
-        private Transform GetPlayerTransform()
+            foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                state[saveable.GetUniqueIdentifier()] = saveable.CaptureState();
+            }
+            return state;
+        }
+
+        private void RestoreState(object state)
+        {
+            Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
+            foreach(SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                saveable.RestoreState(stateDict[saveable.GetUniqueIdentifier()]);
+            }
+        }
+
+        private Transform GetPlayerTransform() // TODO Remove this later!
         {
             return GameObject.FindWithTag("Player").transform;
-        }
-
-
-        private byte[] SerializeVector(Vector3 vector)
-        {
-            byte[] vectorBytes = new byte[3 * 4]; // Three bytes with a typical length of 4.
-            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
-            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
-            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
-
-            return vectorBytes;
-        }
-
-        private Vector3 DeserializeVector(byte[] buffer)
-        {
-            Vector3 result = new Vector3();
-            result.x = BitConverter.ToSingle(buffer, 0);
-            result.y = BitConverter.ToSingle(buffer, 4);
-            result.z = BitConverter.ToSingle(buffer, 8);
-
-            return result;
         }
 
         private string GetPathFromSaveFile(string saveFile)
