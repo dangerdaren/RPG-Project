0b6ee75a6e0a85aa598dc30440d87c614d7e3e62
Daren Stottrup <>
2021-07-02 17:04:25 -0700

    Brain has officially melted. Too much refactoring over the same concept in order to learn "why" but definitely overdone. Lost the plot. Slogging through out of necessity now. I think we did something here.

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Core;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace RPG.Saving
 {
@@ -9,27 +12,41 @@ namespace RPG.Saving
     {
         [SerializeField] string uniqueIdentifier = "";
 
+#if UNITY_EDITOR
         private void Update()
         {
             if (Application.IsPlaying(gameObject)) return;
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
 
-            Debug.Log("Editing");
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
+
+            if (string.IsNullOrEmpty(property.stringValue))
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
+
         }
+#endif
 
         public string GetUniqueIdentifier()
         {
-            return "";
+            return uniqueIdentifier;
         }
 
         public object CaptureState()
         {
-            print($"Capturing state for: {GetUniqueIdentifier()}");
-            return null;
+            return new SerializableVector3(transform.position);
         }
 
         public void RestoreState(object state)
         {
-            print($"Restoring state for: {GetUniqueIdentifier()}");
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
+            GetComponent<ActionScheduler>().CancelCurrentAction();
         }
     }
 }
