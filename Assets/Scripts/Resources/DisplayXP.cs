7296663850523252ef378d0963b87ec8a001d16a
Daren Stottrup <>
2021-07-24 19:37:33 -0700

    Experience points are now save and loadable as well as displayed on the screen via the HUD.

---
+++ Assets/Scripts/Resources/DisplayXP.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace RPG.Resources
+{
+    public class DisplayXP : MonoBehaviour
+    {
+        Experience xp;
+        Text xpText;
+
+        private void Awake()
+        {
+            xp = GameObject.FindWithTag("Player").GetComponent<Experience>();
+            xpText = GetComponent<Text>();
+        }
+
+        private void Update()
+        {
+            ShowXP();
+        }
+
+        private void ShowXP()
+        {
+            xpText.text = String.Format("{0:0}", xp.ExperiencePoints);
+        }
+    }
+}
