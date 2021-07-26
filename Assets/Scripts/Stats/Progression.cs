c3d49e347f484e5db1dc2a06823530c7d04edf80
Daren Stottrup <>
2021-07-25 19:10:03 -0700

    Refactored to use nested dictionaries rather than foreach loops. Thought it'd be less confusing but it was way more. Not sure how to replicate this brain melt, but hope to learn!

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG.Stats
@@ -7,40 +9,54 @@ namespace RPG.Stats
     {
         [SerializeField] CharacterClassProgression[] characterClasses = null;
 
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> classLookupTable = null;
+
         public float GetStat(Stat charStat, CharacterClass charClass, int charLevel)
         {
-            foreach (CharacterClassProgression progressionClass in characterClasses)
+            BuildLookup();
+
+            float[] levels = classLookupTable[charClass][charStat];
+
+            if (levels.Length < charLevel) return 0;
+
+            return levels[charLevel - 1];
+
+        }
+
+        private void BuildLookup()
+        {
+            if (classLookupTable != null) return;
+
+            classLookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
+            foreach (CharacterClassProgression charClassProgression in characterClasses)
             {
-                if (progressionClass.characterClass != charClass) continue;
+                Dictionary<Stat, float[]> statLookupTable = new Dictionary<Stat, float[]>();
 
-                foreach (ProgressionStat progressionStat in progressionClass.stats)
+                foreach (ProgressionStat progressionStat in charClassProgression.stats)
                 {
-                    if (progressionStat.stat != charStat) continue;
-
-
-                    if (progressionStat.levels.Length < charLevel) continue;
-
-                    return progressionStat.levels[charLevel - 1];
+                    statLookupTable[progressionStat.stat] = progressionStat.levels;
                 }
 
+                classLookupTable[charClassProgression.characterClass] = statLookupTable;
+
             }
-            return 0;
         }
 
         [System.Serializable]
         class CharacterClassProgression
         {
             public CharacterClass characterClass;
-            public ProgressionStat[] stats;
+                public ProgressionStat[] stats;
 
         }
 
-        [System.Serializable]
-        class ProgressionStat
-        {
-            public Stat stat;
-            public float[] levels;
-        }
+                [System.Serializable]
+                class ProgressionStat
+                {
+                    public Stat stat;
+                        public float[] levels;
+                }
     }
 }
 
