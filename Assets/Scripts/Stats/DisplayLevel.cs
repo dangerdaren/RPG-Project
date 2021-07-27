9a174c165f088357b3f8ffacbde96b85006b0a2c
Daren Stottrup <>
2021-07-27 16:45:12 -0700

    Somewhere back there I broke Cinemachine when working on the character prefabs. Had to relink them to the FollowCams. All good now!

---
+++ Assets/Scripts/Stats/DisplayLevel.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Stats
+{
+    public class DisplayLevel : MonoBehaviour
+    {
+        GameObject player;
+
+        Text levelText;
+        float charLevel = 1;
+
+        void Awake()
+        {
+            player = GameObject.FindWithTag("Player");
+
+            charLevel = player.GetComponent<BaseStats>().GetLevel();
+            levelText = GetComponent<Text>();
+        }
+
+
+        void Update()
+        {
+            ShowLevel();
+        }
+
+        private void ShowLevel()
+        {
+            charLevel = player.GetComponent<BaseStats>().GetLevel();
+            levelText.text = String.Format("{0:0}", charLevel);
+        }
+    }
+}
