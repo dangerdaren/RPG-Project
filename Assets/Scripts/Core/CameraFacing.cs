7aa395f58a945be74ce067f937c55546ca93cf3a
Daren Stottrup <>
2021-08-10 15:56:17 -0700

    Adding a text UI element to display damage dealt, which will float above the character's head when injured.

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        void Update()
+        {
+            FaceCamera();
+        }
+
+        private void FaceCamera()
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+        [SerializeField] float damage;
+
+        void Start()
+        {
+            Spawn(damage);
+        }
+
+        public void Spawn(float damageAmount)
+        {
+            DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
+        }
+    }
+}
